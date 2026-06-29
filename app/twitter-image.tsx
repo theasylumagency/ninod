bac3786cc67d403016faa6efb68db6f5b57fe6b9
George Orbeladze <george@theasylum.agency>
2026-06-29 13:06:47 +0400

    Initial commit

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,1 +1,6 @@
-export { default, runtime, alt, size, contentType } from "./opengraph-image";
+export { default } from "./opengraph-image";
+
+export const runtime = "nodejs";
+export const alt = "Nino D — Paintings that leave the wall by Nino Devdariani";
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
